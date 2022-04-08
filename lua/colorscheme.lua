716c132e07f5f0204c4070cbebbbf6ade3921022
fgqile <854099812@qq.com>
2022-04-08 12:11:02 +0800

    04/08

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -6,6 +6,9 @@ local colorscheme = "gruvbox"
 -- nord
 -- onedark
 -- nightfox 
+
+--vim.g.tokyonight_style = "night"  --https://github.com/folke/tokyonight.nvim
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme: " .. colorscheme .. " 没有找到！")
